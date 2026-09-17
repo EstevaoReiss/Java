6e9d1ffd8f986211f95f67ce77afb94ecfee0178
estevaocastro <129298689+EstevaoReiss@users.noreply.github.com>
2026-09-17 00:33:43 -0300

    fix: update JanelaBorderLayout

--- ControleDeCartas_v1/src/br/unip/sicc/view/JanelaBorderLayout.java
+++ ControleDeCartas_v1/src/br/unip/sicc/view/JanelaBorderLayout.java
@@ -1,7 +1,11 @@
 package br.unip.sicc.view;
 
 import java.awt.BorderLayout;
+import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
@@ -18,37 +22,60 @@ public class JanelaBorderLayout extends JFrame {
 		this.add(painelCadastro, BorderLayout.WEST);
 		this.add(painelBusca, BorderLayout.CENTER);
 		
-		// titulo da janela;
+		// Monta a barra de menus
+		this.setJMenuBar(montaMenu());
+		
+		// Título da janela
 		this.setTitle("Controle de Cartas");
-		// tamanho da janela;
+		
+		// Tamanho da janela
 		this.setSize(800, 300);
-		//encerramento da janela;
+		
+		// Encerramento da janela
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		//Centralizando a Janela ;
+		
+		// Centralizando a janela
 		this.setLocationRelativeTo(null);
 		
 	}
 	
 	private JPanel montaPainelBusca() {
-
-		
 		return new PainelBusca();
 	}
 	
-	
 	private JPanel montaPainelCadastro() {
-	
 		return new PainelCadastro();
 	}
 	
-	
+	private JMenuBar montaMenu() {
+		
+		JMenuBar barraMenu = new JMenuBar();
+		
+		JMenu menuCadastro = new JMenu("Cadastro");
+		menuCadastro.setMnemonic(KeyEvent.VK_T);
+		
+		JMenuItem itemNovo = new JMenuItem("Novo");
+		itemNovo.setMnemonic(KeyEvent.VK_N);
+		
+		menuCadastro.add(itemNovo);
+		
+		JMenu menuAjuda = new JMenu("Ajuda");
+		menuAjuda.setMnemonic(KeyEvent.VK_A);
+		
+		JMenuItem itemSobre = new JMenuItem("Sobre");
+		itemSobre.setMnemonic(KeyEvent.VK_S);
+		
+		menuAjuda.add(itemSobre);
+		
+		barraMenu.add(menuCadastro);
+		barraMenu.add(menuAjuda);
+		
+		return barraMenu;
+	}
 	
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(() -> {
 			new JanelaBorderLayout().setVisible(true);
 		});
 	}
-	
-	
-	
 }
