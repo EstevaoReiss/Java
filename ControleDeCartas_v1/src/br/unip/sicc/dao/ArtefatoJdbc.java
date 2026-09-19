95ef7f67cdc3e0178019df0eb6035865bcd37af0
EstevaoReiss <129298689+EstevaoReiss@users.noreply.github.com>
2026-09-18 21:25:01 -0300

    feat: att news jdbc task

---
+++ ControleDeCartas_v1/src/br/unip/sicc/dao/ArtefatoJdbc.java
@@ -0,0 +1,11 @@
+package br.unip.sicc.dao;
+
+public class ArtefatoJdbc implements ArtefatoDao {
+
+    private static final String SQL_DELETE
+            = "DELETE FROM TB_ARTEFATO WHERE ID = ?;";
+
+    @Override 
+     
+    
+}
